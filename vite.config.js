30dd38396fbc2b96ff69ac36e4da67bfc1cccbf8
Alas06012 <diegoalas06@gmail.com>
2024-10-07 19:57:45 -0600

    test

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://alas06012.github.io/KodigoMusicSPA"
 })
